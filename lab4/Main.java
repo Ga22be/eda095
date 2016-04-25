fc99be17a54f0d5b75796ea4d2ad5932baa04aa5
Johan Davidsson <johan.et.davidsson@gmail.com>
2016-04-25 18:44:21 +0200

    add lab4

---
+++ lab4/Main.java
@@ -0,0 +1,39 @@
+package lab4;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		URL startURL = null;
+
+		try {
+			startURL = new URL("http://cs.lth.se/eda095");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// number of links
+		int nbrLinks = 100;
+
+		Spider spider = new Spider(nbrLinks);
+
+		// 10 thread for now
+		int nbrThreads = 10;
+
+		// create threads and start threads
+
+		for (int i = 0; i < nbrThreads; i++) {
+			Processor processor = new Processor(spider);
+			processor.start();
+		}
+
+		spider.init(startURL);
+
+		
+	}
+
+}
